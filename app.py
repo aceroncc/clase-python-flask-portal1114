0c6ffd6618372e2c3be8a15d0d0be9d405ce4ee5
aceroncc <aceronc867@inemkennedy.edu.co>
2026-06-04 13:45:35 -0500

    hi

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
-# Importamos Flask y una funcion que permite mostrar un HTML.
 from flask import Flask, render_template
 
-
-# Creamos la aplicacion principal.
-# Este objeto sera el centro de nuestro proyecto Flask.
 app = Flask(__name__)
 
-
-# Cuando alguien entra a la direccion principal del sitio, Flask ejecuta
-# esta funcion y devuelve la pagina `index.html`.
 @app.route("/")
+
 def inicio():
-    # `render_template` busca archivos dentro de la carpeta `templates`.
-    return render_template("index.html")
-# Este bloque se ejecuta solo si corremos `python app.py` desde la terminal.
-if __name__ == "__main__":
-    # `debug=True` sirve en desarrollo porque reinicia el servidor
-    # cuando detecta cambios y muestra errores con mas detalle.
+    nombre_profesor = "Henry Ortegon"
+    email_profesor = "henry@kyrbot.com"
+    horario = "Miercoles 16:45-18:10 | Jueves 12:30-14:20"
+    aula = "215"
+    descripcion = "Aprenderemos Python, Flask y construiremos un portal web real"
+
+    return render_template(
+    "index.html",
+    profesor=nombre_profesor,
+    horario=horario
+)
+
+@app.route("/informacion")
+def informacion():
+datos = {
+"aula": "215",
+"profesor": "Henry Ortegon",
+"horario": "Miercoles 16:45-18:10 | Jueves 12:30-14:20"
+}
+
+return render_template("info.html", **datos)
+
+@app.route("/recursos")
+def recursos():
+return render_template("recursos.html")
+
+@app.route("/tareas")
+def tareas():
+return render_template("tareas.html")
+
+if __name__ == "main":
     app.run(debug=True)
